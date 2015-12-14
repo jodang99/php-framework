34da7b7229476f0650066532730d3ff3f076fb03
1002602 <1002602@SKP1002602MN001.local>
2015-12-14 18:09:56 +0900

    sss

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -8,6 +8,7 @@ abstract class Controller{
 	protected $response;
 	protected $session;
 	protected $db_manager;
+	protected $auth_actions = array();
 	
 	public function __construct($application) {
 		$this->controller_name = strtolower(substr(get_class($this),0,-10));
@@ -28,8 +29,21 @@ abstract class Controller{
 			$this->forward404();
 		}
 		
+		if ($this->needsAuthentication($action) && !$this->session->isAuthenticated())
+		{
+			throw new UnauthorizedActionException();
+		}
+		
 		$content = $this->$action_method($params);//$this->$ac... 가변함수(실제실행)
 		return $content;
+	}
+	
+	protected function needsAuthentication($action)
+	{
+		if ($this->auth_actions === true || (is_array($this->auth_actions) && in_array($action,$this->auth_actions))){
+			return true;
+		}
+		return false;
 	}
 	
 	protected function render($variables = array(),$template = null, $layout='layout')
@@ -52,6 +66,48 @@ abstract class Controller{
 	protected function forward404()
 	{
 		throw new HttpNotFoundException('Forwarded 404 page from '.$this->controller_name,'/'.$this->action_name);
+	}
+	
+	protected function redirect($url)
+	{
+		if (!preg_match('#https?://#', $url)) {
+			$protocl = $this->request->isSsl() ? 'https://' : 'http://';
+			$host = $this->request->getHost();
+			$base_url = $this->request->getBaseUrl();
+			
+			$url = $protocl.$host.$base_url.$url;
+		}
+		$this->response->setStatus(302,'Found');
+		$this->response->setHttpHeader('Location',$url);
+	}
+	
+	protected function generateCsrfToken($form_name)
+	{
+		$key = 'csrf_tokens/'.$form_name;
+		$tokens = $this->session->get($key,array());
+		if (count($tokens) > 10){
+			array_shift($tokens);
+		}
+		
+		$token = sha1($form_name.session_id().microtime());
+		$tokens[] = $token;
+		
+		$this->session->set($key,$tokens);
+		return $token;
+	}
+	
+	protected function checkCsrfToken($form_name,$token)
+	{
+		$key = 'csrf_tokens/'.$from_name;
+		$tokens = $this->session->get($key,array());
+		
+		if (false !== ($pos = array_search($token,$tokens,true))){
+			unset($tokens[$pos]);
+			$this->session->set($key,$tokens);
+			
+			return true;
+		}
+		return false;
 	}
 }
 
