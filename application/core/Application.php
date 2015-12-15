2ab06c1b87f47a6d5821b5aaaeb8d3dab2abf3be
1002602 <1002602@SKP1002602MN001.local>
2015-12-15 13:28:17 +0900

    last frame

--- application/core/Application.php
+++ application/core/Application.php
@@ -8,6 +8,7 @@ abstract class Application
 	protected $session;
 	protected $db_manager;
 	protected $router;
+	protected $login_action = array();
 	
 	public function __construct($debug = false)
 	{
@@ -62,7 +63,10 @@ abstract class Application
 			$this->runAction($controller,$action,$params);
 		} catch (HttpNotFoundException $e) {
 			$this->render404Page($e);
-		}		
+		} catch (UnauthorizedActionException $e){
+			list($controller,$action) = $this->login_action;
+			$this->runAction($controller,$action);
+		}
 		$this->response->send();
 	}
 	
